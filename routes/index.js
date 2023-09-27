d19a556e7ebe72a5cdcdb602c369e0ff048dcb0b
Ngece <ngecemlamli@gmail.com>
2023-09-27 18:49:10 +0200

    committing index.js

--- routes/index.js
+++ routes/index.js
@@ -1,42 +1,27 @@
+import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
+
 const express = require('express');
+// all endpoints of our API
+const router = (app) => {
+  const paths = express.Router();
+  app.use(express.json());
+  app.use('/', paths);
 
-const router = express.Router();
+  paths.get('/status', ((request, response) => AppController.getStatus(request, response)));
+  paths.get('/stats', ((request, response) => AppController.getStats(request, response)));
+  paths.post('/users', ((request, response) => UsersController.postNew(request, response)));
+  paths.get('/connect', ((request, response) => AuthController.getConnect(request, response)));
+  paths.get('/disconnect', ((request, response) => AuthController.getDisconnect(request, response)));
+  paths.get('/users/me', ((request, response) => UsersController.getMe(request, response)));
+  paths.post('/files', ((request, response) => FilesController.postUpload(request, response)));
+  paths.get('/files/:id', ((request, response) => FilesController.getShow(request, response)));
+  paths.get('/files', ((request, response) => FilesController.getIndex(request, response)));
+  paths.put('/files/:id/publish', ((request, response) => FilesController.putPublish(request, response)));
+  paths.put('/files/:id/unpublish', ((request, response) => FilesController.putUnpublish(request, response)));
+  paths.get('/files/:id/data', ((request, response) => FilesController.getFile(request, response)));
+};
 
-const AppController = require('../controllers/AppController');
-const AuthController = require('../controllers/AuthController');
-const UsersController = require('../controllers/UsersController');
-const FilesController = require('../controllers/FilesController');
-
-// The /status endpoint
-router.get('/status', AppController.getStatus);
-
-// The /stats endpoint
-router.get('/stats', AppController.getStats);
-
-// Define the /connect endpoint
-router.get('/connect', AuthController.getConnect);
-
-// Define the /disconnect endpoint
-router.get('/disconnect', AuthController.getDisconnect);
-
-// Define the /users/me endpoint
-router.get('/users/me', UsersController.getMe);
-
-// Define the /users endpoint
-router.post('/users', UsersController.postNew);
-
-// Define the /files endpoint
-router.post('/files', FilesController.postUpload);
-
-// Define the Get and List endpoints
-router.get('/files/:id', FilesController.getShow);
-router.get('/files', FilesController.getIndex);
-
-// Define publish and un-publish endpoints
-router.put('/files/:id/publish', FilesController.putPublish);
-router.put('/files/:id/unpublish', FilesController.putUnpublish);
-
-// Define the /files/:id/data endpoint
-router.get('/files/:id/data', FilesController.getFile);
-
-module.exports = router;
+export default router;
